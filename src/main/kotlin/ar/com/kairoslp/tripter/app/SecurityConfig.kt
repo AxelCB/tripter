8bb01103ed7bf317a1466af2e25b22517fb1258a
Axel Collard Bovy <axcbovy@gmail.com>
2019-04-02 21:12:28 -0300

    Added login and session configurations through spring security

--- src/main/kotlin/ar/com/kairoslp/tripter/app/SecurityConfig.kt
+++ src/main/kotlin/ar/com/kairoslp/tripter/app/SecurityConfig.kt
@@ -1,32 +1,40 @@
 package ar.com.kairoslp.tripter.app
 
+import ar.com.kairoslp.tripter.service.UserDetailsService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 
 @EnableWebSecurity
-class SecurityConfig: WebSecurityConfigurerAdapter() {
+class SecurityConfig(@Autowired val userDetailsService: UserDetailsService): WebSecurityConfigurerAdapter() {
 
     @Throws
     override fun configure(http: HttpSecurity) {
         http
                 .authorizeRequests()
-                .antMatchers("/css/**", "/index", "/register").permitAll()
-                .antMatchers("/traveler/**").permitAll()
-//                .antMatchers("/traveler/**").hasRole("USER")
+                .antMatchers("/css/**", "/index", "/user/register").permitAll()
+                .antMatchers("/traveler/**").hasRole("USER")
+                .anyRequest().authenticated()
                 .and()
                 .formLogin()
 //        .loginPage("/login").failureUrl("/login-error")
-
+                .and().csrf().disable()
     }
 
     @Autowired
     @Throws
     fun configureGlobal(auth: AuthenticationManagerBuilder) {
         auth
-                .inMemoryAuthentication()
-                .withUser("admin").password("123456").roles("USER")
+            .userDetailsService(userDetailsService).passwordEncoder(passwordEncoder())
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
     }
 }
---
+++ src/main/kotlin/ar/com/kairoslp/tripter/service/UserDetailsService.kt
@@ -0,0 +1,25 @@
+package ar.com.kairoslp.tripter.service
+
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.stereotype.Service
+import ar.com.kairoslp.tripter.persistence.repository.UserRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import javax.transaction.Transactional
+
+
+@Service
+class UserDetailsService(@Autowired private val userRepository: UserRepository): UserDetailsService {
+
+    @Transactional
+    override fun loadUserByUsername(username: String): UserDetails {
+        val user = userRepository.findByUsername(username) ?: throw UsernameNotFoundException(username)
+
+        val grantedAuthorities = HashSet<SimpleGrantedAuthority>()
+        grantedAuthorities.add(SimpleGrantedAuthority("TRAVELER"))
+
+        return org.springframework.security.core.userdetails.User(user.email, user.password, grantedAuthorities)
+    }
+}
