d172d1aafef3fec9903ce9100fc59a9daf67d53a
Axel Collard Bovy <axcbovy@gmail.com>
2019-04-06 16:03:00 -0300

    Removed empty class bodies where they weren't used

--- src/main/kotlin/ar/com/kairoslp/tripter/persistence/repository/TravelerNetworkRepository.kt
+++ src/main/kotlin/ar/com/kairoslp/tripter/persistence/repository/TravelerNetworkRepository.kt
@@ -3,5 +3,4 @@ package ar.com.kairoslp.tripter.persistence.repository
 import ar.com.kairoslp.tripter.model.TravelerNetwork
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface TravelerNetworkRepository: JpaRepository<TravelerNetwork, Long> {
-}
+interface TravelerNetworkRepository: JpaRepository<TravelerNetwork, Long>
