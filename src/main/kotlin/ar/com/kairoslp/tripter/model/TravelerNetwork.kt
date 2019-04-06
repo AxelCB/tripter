0a91f8c40262f9b04333624f1dd506b82154d3f2
Axel Collard Bovy <axcbovy@gmail.com>
2019-04-06 15:54:28 -0300

    Updated traveler network to avoid using an extra table

--- src/main/kotlin/ar/com/kairoslp/tripter/model/TravelerNetwork.kt
+++ src/main/kotlin/ar/com/kairoslp/tripter/model/TravelerNetwork.kt
@@ -2,19 +2,21 @@ package ar.com.kairoslp.tripter.model
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import java.io.Serializable
-import java.util.ArrayList
+import java.util.*
 import javax.persistence.*
 
 @Entity
 @JsonIgnoreProperties("hibernateLazyInitializer", "handler")
 class TravelerNetwork(@Id @GeneratedValue val id: Long? = null,
-                      @OneToMany(cascade = [(CascadeType.ALL)], orphanRemoval = true) var users: MutableList<User> = ArrayList()): Serializable {
+                      @OneToMany(cascade = [(CascadeType.ALL)], orphanRemoval = true)
+                      @JoinColumn(name = "traveler_network_id")
+                      var users: MutableList<User> = ArrayList()): Serializable {
 
     fun addUser(user: User) {
         this.users.add(user)
     }
 
     fun getUserById(userId: Long): User {
-        return this.users.filter {user -> user.id == userId  }.first()
+        return this.users.first { user -> user.id == userId }
     }
 }
